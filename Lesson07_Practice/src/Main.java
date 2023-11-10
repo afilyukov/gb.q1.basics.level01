d125c18ecb422cfe01b96e1a143571d37667dde3
veles <filyukvo@yandex.ru>
2023-11-10 20:43:49 +0300

    lesson07 fixes 2

--- Lesson07_Practice/src/Main.java
+++ Lesson07_Practice/src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         Cat cat = new Cat("Barsik", 5);
-        Plate plate = new Plate(100);
+        Plate plate = new Plate(100, 100);
         plate.info();
         cat.eat(plate);
         plate.info();
